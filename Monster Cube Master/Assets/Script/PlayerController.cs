72a7f59247f97f3c3c2f6e214a25fd239cef2a47
ws723514 <nathanielwang521@gmail.com>
2021-01-22 19:02:29 -0800

    Merge try

---
+++ Monster Cube Master/Assets/Script/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 4f;
+    float sprintspeed = 10f;
+    float gravity = 1f;
+    float jumpSpeed = 10f;
+
+    float energy = 10f;
+
+    Vector3 movedirection;
+    CharacterController mycontroller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mycontroller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+    void move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        if(mycontroller.isGrounded)
+        {
+            movedirection = new Vector3(moveX, 0, moveZ);
+            movedirection = transform.TransformDirection(movedirection);
+
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                movedirection.y += jumpSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift) && moveZ == 1)
+            {
+                movedirection *= sprintspeed;
+
+
+            }else {
+                movedirection *= speed;
+            }
+          
+        }
+
+        movedirection.y -= gravity;
+        //make sure different frame got same speed;
+        mycontroller.Move(movedirection * Time.deltaTime);
+
+    }
+}
