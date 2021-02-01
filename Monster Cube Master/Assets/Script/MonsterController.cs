d990b8aa961bc48ecbd353a0b2127162661f8800
annasalieva <asalieva@uci.edu>
2021-02-01 03:13:08 -0800

    added monster prefab + movement control script

---
+++ Monster Cube Master/Assets/Script/MonsterController.cs
@@ -0,0 +1,32 @@
+﻿// OG AUTHOR: ANNA
+// Moves the Monster based on the player's input of the arrow keys
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        //establishes 4 directional movement plus rotation
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            //go backwards
+        }
+
+
+    }
+}
