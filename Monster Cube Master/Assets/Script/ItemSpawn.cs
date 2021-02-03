1e0892ba7736f8ba5c6d8cc5076ebb13b08bd9ef
Tyler Pasela <paselat99@gmail.com>
2021-02-02 20:35:20 -0800

    Added a map variant with spawns

---
+++ Monster Cube Master/Assets/Script/ItemSpawn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+
+    public GameObject[] foodLocations;
+    public GameObject food;
+
+    public GameObject[] waterLocations;
+    public GameObject water;
+
+    private bool[] w;
+    private bool[] f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodLocations = GameObject.FindGameObjectsWithTag("FoodSpawn");
+        waterLocations = GameObject.FindGameObjectsWithTag("WaterSpawn");
+
+        w = new bool[waterLocations.Length];
+        f = new bool[foodLocations.Length];
+
+        spawnWater();
+        spawnFood();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnWater()
+    {
+        for (int i = 0; i<waterLocations.Length; i++){
+            GameObject.Instantiate(water, waterLocations[i].transform.position, Quaternion.identity);
+        }
+    }
+
+    private void spawnFood()
+    {
+        for (int i = 0; i<foodLocations.Length; i++){
+            GameObject.Instantiate(food, foodLocations[i].transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Monster Cube Master/Assets/Script/KeySpawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySpawn : MonoBehaviour
+{
+    public GameObject[] keyLocations;
+    public GameObject key;
+
+    private bool[] keys;
+    void Start()
+    {
+        keyLocations = GameObject.FindGameObjectsWithTag("KeySpawn");
+        keys = new bool[keyLocations.Length];
+        for (int i = 0; i<keyLocations.Length; i++){
+            keys[i] = false;
+        }
+        spawnKey();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnKey()
+    {
+        int count = 0;
+        while(true){
+            if (count == 3){
+                break;
+            }
+            int spawn = Random.Range(0, keyLocations.Length);
+            if (keys[spawn]){
+                continue;
+            }
+            else{
+                GameObject.Instantiate(key, keyLocations[spawn].transform.position, Quaternion.identity);
+                keys[spawn] = true;
+                count +=1;
+            }
+        }
+    }
+}
