8b0b71daf81c5123cad822f1d5ae7c0014d578f3
annasalieva <asalieva@uci.edu>
2021-02-19 17:56:23 -0800

    monster camera + monster hit

---
+++ Monster Cube Master/Assets/monsterHit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterHit : MonoBehaviour
+
+    
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+          other.GetComponent<StatusDecrease>().healthSlider.value -= 50; //decrease player health by 50 (out of 100)
+
+        }
+    }
+}
