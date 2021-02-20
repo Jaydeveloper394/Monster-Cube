f4b8659d58dbfbbf46e36036e971ee1cf3f822fa
Jaydeveloper394 <jaydevp394@gmail.com>
2021-02-19 18:32:09 -0800

    Health now drops gradually if thirst/hunger is 0 (and does not drop if both are not 0)

--- Monster Cube Master/Assets/Script/Nate/InventoryController.cs
+++ Monster Cube Master/Assets/Script/Nate/InventoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class InventoryController : MonoBehaviour
@@ -42,5 +43,110 @@ public class InventoryController : MonoBehaviour
 
         }
 
+    }
+    
+    public void UseItem(int itemSlot)
+    {
+       //IInventoryItem tempItem = mItems[itemSlot - 1];
+       
+/*        //food to recover hunger status
+       if(tempItem.gameObject.tag == "Food")
+       {
+          
+       }
+       
+       //water to recover thirst status
+       if(tempItem.gameObject.tag == "Water")
+       {
+          
+       }
+       
+       //delete item from list (if not an important item like a key)
+       if(tempItem.gameObject.tag != "Key")
+       {
+          
+       } */
+    }
+    
+    public void DropItem(int itemSlot)
+    {
+       //IInventoryItem tempItem = mItems[itemSlot - 1];
+       
+       //Delete item from inventory and Re-instantiate item to player's current location
+    }
+    
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.Alpha1))
+       {
+          if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+          {
+             DropItem(1);
+          }
+          else
+          {
+             UseItem(1);
+          }
+       }
+       
+       if(Input.GetKey(KeyCode.Alpha2))
+       {
+          if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+          {
+             DropItem(2);
+          }
+          else
+          {
+             UseItem(2);
+          }
+       }
+       
+       if(Input.GetKey(KeyCode.Alpha3))
+       {
+          if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+          {
+             DropItem(3);
+          }
+          else
+          {
+             UseItem(3);
+          }
+       }
+       
+       if(Input.GetKey(KeyCode.Alpha4))
+       {
+          if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+          {
+             DropItem(4);
+          }
+          else
+          {
+             UseItem(4);
+          }
+       }
+       
+       if(Input.GetKey(KeyCode.Alpha5))
+       {
+          if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+          {
+             DropItem(5);
+          }
+          else
+          {
+             UseItem(5);
+          }
+       }
+       
+       if(Input.GetKey(KeyCode.Alpha6))
+       {
+          if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+          {
+             DropItem(6);
+          }
+          else
+          {
+             UseItem(6);
+          }
+       }
     }
 }
--- Monster Cube Master/Assets/Script/StatusDecrease.cs
+++ Monster Cube Master/Assets/Script/StatusDecrease.cs
@@ -13,18 +13,19 @@ public class StatusDecrease : MonoBehaviour
     public float thirstDropRate;
     public float hungerDropRate;
     
-    public void decreaseHealth()
-    {
-       StartCoroutine(DecreaseSlider(healthSlider, healthDropRate));
-    }
+    private bool thirstIsDropping;
+    private bool hungerIsDropping;
     
     public void decreaseThirst()
     {
+       thirstIsDropping = true;
        StartCoroutine(DecreaseSlider(thirstSlider, thirstDropRate));
+
     }
     
     public void decreaseHunger()
     {
+       hungerIsDropping = true;
        StartCoroutine(DecreaseSlider(hungerSlider, hungerDropRate));
     }
     
@@ -32,13 +33,16 @@ public class StatusDecrease : MonoBehaviour
      {
          if (slider != null)
          {
-            float dropValue = (slider.value / 100);
+            float dropValue = (slider.maxValue / 1000);
             while (slider.value >= 0)
             {
              slider.value -= dropValue;
              yield return new WaitForSeconds(1 / dropRate);
-             if (slider.value <= 0)
-                  break;
+             if(slider.value <= 0)
+             {
+                slider.value = 0.0f;
+                break;
+             }
             }
          }
          yield return null;
@@ -47,12 +51,77 @@ public class StatusDecrease : MonoBehaviour
    
     void Start()
     {
+        hungerIsDropping = true;
         decreaseHunger();
+        
+        thirstIsDropping = true;
         decreaseThirst();
     }
 
     void Update()
     {
+        if(thirstSlider.value <= 0)
+        {
+           thirstIsDropping = false;
+           float dropValue = (healthSlider.maxValue * healthDropRate / 10000);
+           healthSlider.value -= dropValue;
+        }
+        
+        //if both thirst and hunger are 0, health drops at double the speed
+        if(hungerSlider.value <= 0)
+        {
+           hungerIsDropping = false;
+           float dropValue = (healthSlider.maxValue * healthDropRate / 10000);
+           healthSlider.value -= dropValue;
+        }
+        
+        //after regaining thirst / hunger from an item, continue dropping
+        if(!thirstIsDropping && thirstSlider.value > 0)
+        {
+           decreaseThirst();
+        }
+        
+        if(!hungerIsDropping && hungerSlider.value > 0)
+        {
+           decreaseHunger();
+        }
+        
+        if(healthSlider.value <= 0)
+        {
+           Debug.Log("Player health dropped to 0");
+        }
+        
+        
+        //only for debugging purposes
+        if(Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Alpha4))
+        {
+           healthSlider.value += 0.2f;
+           if(healthSlider.value > healthSlider.maxValue)
+           {
+              healthSlider.value = healthSlider.maxValue;
+           }
+        }
+        
+        //only for debugging purposes
+        if(Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Alpha5))
+        {
+           thirstSlider.value += 0.3f;
+           if(thirstSlider.value > thirstSlider.maxValue)
+           {
+              thirstSlider.value = thirstSlider.maxValue;
+           }
+        }
+        
+        //only for debugging purposes
+        if(Input.GetKey(KeyCode.Alpha3) || Input.GetKey(KeyCode.Alpha6))
+        {
+           hungerSlider.value += 0.2f;
+           if(hungerSlider.value > hungerSlider.maxValue)
+           {
+              hungerSlider.value = hungerSlider.maxValue;
+           }
+        }
+        
         
     }
 }
