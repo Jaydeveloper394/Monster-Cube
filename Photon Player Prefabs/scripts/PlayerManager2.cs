10424bb4259340a76dc8e5a0467178f5dc57f699
JakeYamash <63979658+JakeYamash@users.noreply.github.com>
2021-02-24 17:20:16 -0800

    Add files via upload

---
+++ Photon Player Prefabs/scripts/PlayerManager2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager2 : MonoBehaviour
+{
+    // Instaniates player controller object with Photon View making sure that it is only creating one for the owner/individual players
+    //used to keep persistent player data
+    //and recive info from other platers
+    PhotonView PV;
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    // Update is called once per frame
+    void CreateController()
+    {
+        Vector3 spawn_pos = new Vector3(0, 1, 0);
+        Debug.Log("Instantiated Player Controller");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerTest"), spawn_pos, Quaternion.identity);
+    }
+}
