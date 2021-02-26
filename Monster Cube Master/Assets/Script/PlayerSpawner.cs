62bd22846e4982183f92aec5182491218ee9c12b
annasalieva <asalieva@uci.edu>
2021-02-26 00:44:12 -0800

    player spawner added to game manager

---
+++ Monster Cube Master/Assets/Script/PlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+//Anna S.
+//This script is used to spawn players and monster
+public class PlayerSpawner : MonoBehaviour
+{
+    //private variables
+    private int playerNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNum = PhotonNetwork.CurrentRoom.PlayerCount;
+        Debug.Log(playerNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
