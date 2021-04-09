1e38d7d6826b654f222b0a0f397216b95c99efb9
Jaydeveloper394 <jaydevp394@gmail.com>
2021-04-09 14:05:35 -0700

    Removed Food/Water Status and added Countdown Timer (300 sec.)

---
+++ Monster Cube Master/Assets/Script/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float currentTime = 0.0f;
+    public float startTime = 300.0f;
+
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = "Time Remaining: " + currentTime.ToString("0");
+
+        if(currentTime <= 0.0f)
+        {
+            currentTime = 0.0f;
+            //Time's up for the player (Monster wins).
+        }
+    }
+}
